7fc6e87c8234b14692f9568252af09dcbea14a04
Theodor Kvalsvik Lauritzen <theodor.k.lauritzen@gmail.com>
2020-08-30 18:33:59 +0200

    Preparing to deploy to Heroku

--- routes/user.js
+++ routes/user.js
@@ -3,15 +3,24 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/login', function(req, res, next) {
-  const redirect_url = "http://localhost/user/success";
+  const redirect_url = `${req.protocol}://${req.get('host')}/user/oauth/response`;
   const scope = "offline_access%20user.read%20notes.read.all";
   // TODO: Add a state for security
   // See: https://docs.microsoft.com/en-us/graph/auth-v2-user?view=graph-rest-1.0
   res.redirect(`https://login.microsoftonline.com/${process.env.OATH_TENANT}/oauth2/v2.0/authorize?client_id=${process.env.OAUTH_CLIENT_ID}&response_type=code&redirect_uri=${redirect_url}&response_mode=query&scope=${scope}`);
 });
 
-router.get("/oathresponse", (req, res, next) => {
-  res.send(req);
+router.get("/oauth/response", (req, res, next) => {
+  res.send(req.url)
 })
 
 module.exports = router;
+
+// https://portal.azure.com/#home
+
+// https://docs.microsoft.com/en-us/graph/onenote-get-content
+// https://docs.microsoft.com/en-us/graph/auth-v2-user?view=graph-rest-1.0
+
+// https://portal.office.com/account/?ref=MeControl#apps
+
+// https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-protocols-oidc
